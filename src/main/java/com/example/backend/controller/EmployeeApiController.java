4f3e1973325cfdadf969520639abf375b657cb18
integer <integer402@gmail.com>
2022-12-30 14:47:19 +0900

    MyBatis employee getEmployeeList 완성

--- src/main/java/com/example/backend/controller/EmployeeApiController.java
+++ src/main/java/com/example/backend/controller/EmployeeApiController.java
@@ -1,8 +1,12 @@
 package com.example.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +20,20 @@ public class EmployeeApiController {
 	@Autowired
 	private EmployeeServiceImpl employeeService;
 	
+	@GetMapping("/emplist")
+	public List<EmployeeDto> getEmployeeList(@RequestBody EmployeeDto dto){
+		return employeeService.getEmployeeList(dto);
+	}
+	
 	@GetMapping("/{employeeId}")
 	public EmployeeDto getEmployee(@PathVariable(required = true) int employeeId) {
 		return employeeService.getEmployee(employeeId);
 	}
+
+//	@PostMapping("/emplist")
+//	public List<EmployeeDto> getEmployeeList(@RequestBody EmployeeDto dto) {
+//		System.out.println(dto.toString());
+//		return employeeService.getEmployeeList(dto);
+//	}
+
 }
