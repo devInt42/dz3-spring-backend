07756fe08f9ebda6200b41e717b8494dfc5be7e1
integer <integer402@gmail.com>
2023-01-02 11:31:01 +0900

    employee CRD 완성

--- src/main/java/com/example/backend/controller/EmployeeApiController.java
+++ src/main/java/com/example/backend/controller/EmployeeApiController.java
@@ -1,10 +1,12 @@
 package com.example.backend.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,20 +24,36 @@ public class EmployeeApiController {
 	@Autowired
 	private EmployeeServiceImpl employeeService;
 	
-//	@GetMapping("/emplist")
-//	public List<EmployeeDto> getEmployeeList(EmployeeDto dto){
-//		return employeeService.getEmployeeList(dto);
-//	}
-	
-	@GetMapping("/{employeeId}")
-	public EmployeeDto getEmployee(@PathVariable(required = true) int employeeId) {
-		return employeeService.getEmployee(employeeId);
-	}
-
-	@PostMapping("/list")
-	public List<EmployeeDto> getEmployeeList(EmployeeDto dto) {
-		System.out.println(dto.toString());
+	// 전체 사원 조회
+	@GetMapping("/emplist")
+	public List<EmployeeDto> getEmployeeList(EmployeeDto dto){
 		return employeeService.getEmployeeList(dto);
 	}
+	
+	// 사번으로 사원 조회
+	@GetMapping("/emplist/{employeeId}")
+	public EmployeeDto getEmployee(@PathVariable(required = true) int employeeId) {
+		return employeeService.getEmployeeById(employeeId);
+	}
+	
+	// 사원 추가
+	@PostMapping
+	public void addEmployee(@RequestBody(required=true) Map<String, String> map) {
+		employeeService.addEmployee(map);
+	}
+	
+	// 삭제
+	@DeleteMapping("/emplist/{employeeId}")
+	public void removeEmployee(@PathVariable(required=true)  int employeeId) {
+		employeeService.removeEmployee(employeeId);
+	}
 
+	// 수정
+	@PatchMapping("/emplist/{employeeId}")
+	public void setEmployee(@PathVariable(required=true) String employeeId, @RequestBody(required=true) Map<String, String> map) {
+		map.put("employeeId", employeeId);
+		System.out.println(map);
+		employeeService.setEmployee(map);
+	}
+	
 }
