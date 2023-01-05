4f67c9de4f6946e1772d61b7a0f37fefa5f40ca3
integer <integer402@gmail.com>
2023-01-05 11:18:55 +0900

    컨트롤러 초기 생성

---
+++ src/main/java/com/example/backend/controller/mapping/CompanyEmployeeApiController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller.mapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/company-employee")
+public class CompanyEmployeeApiController {
+
+}
