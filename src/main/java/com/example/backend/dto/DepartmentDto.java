cc1cb4304a6137f99b4452bf93203df92756f815
integer <integer402@gmail.com>
2023-01-05 12:06:07 +0900

    오탈자 수정

--- src/main/java/com/example/backend/dto/DepartmentDto.java
+++ src/main/java/com/example/backend/dto/DepartmentDto.java
@@ -1,17 +1,18 @@
 package com.example.backend.dto;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class DepartmentDto {
-	private int workplaceSeq;
+	private int departementSeq;
 	private int companySeq;
-	private int workplaceCode;
-	private String workplaceName;
-	private String workplaceAddr;
-	private int workplacePresident;
-	private String workplaceCall;
-	private Date workplaceEmail;
-}
+	private int workplaceSeq;
+	private int departmentCode;
+	private String departmentName;
+	private int departmentPresident;
+	private String departmentCategory;
+	private String departmentCall;
+	private String departmentLoc;
+	private int departmentParent;
+	private int flag;
+	}
