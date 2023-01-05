bb3fa6f633522487133c0b08b18e94df7b94f75d
integer <integer402@gmail.com>
2023-01-05 12:07:46 +0900

    serviceImpl 초기설정

---
+++ src/main/java/com/example/backend/service/AuthMenuServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.backend.service;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthMenuServiceImpl implements AuthMenuService {
+//	@Autowired
+//	private SqlSessionTemplate sqlSession;
+
+	
+
+}
--- src/main/java/com/example/backend/service/ExampleServiceImpl.java
+++ src/main/java/com/example/backend/service/ExampleServiceImpl.java
@@ -8,43 +8,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.backend.dto.EmployeeDto;
-import com.example.backend.mapper.EmployeeMapper;
+import com.example.backend.mapper.ExampleMapper;
 
 @Component
-public class EmployeeServiceImpl implements EmployeeService {
+public class ExampleServiceImpl implements ExampleService {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 
 	@Override
 	public EmployeeDto getEmployeeByCode(int employeeCode) {
 		// TODO Auto-generated method stub
-		return sqlSession.getMapper(EmployeeMapper.class).getByCode(employeeCode);
+		return sqlSession.getMapper(ExampleMapper.class).getByCode(employeeCode);
 	}
 
 	@Override
 	public List<EmployeeDto> getEmployeeList(EmployeeDto dto) {
 		// TODO Auto-generated method stub
 		System.out.println(dto);
-		return sqlSession.getMapper(EmployeeMapper.class).getList(dto);
+		return sqlSession.getMapper(ExampleMapper.class).getList(dto);
 
 	}
 
 	// delete
 	@Override
 	public void removeEmployee(int employeeCode) {
-		sqlSession.getMapper(EmployeeMapper.class).remove(employeeCode);
+		sqlSession.getMapper(ExampleMapper.class).remove(employeeCode);
 	}
 	
 	// update set
 	@Override
 	public void setEmployee(Map<String, String> map) {
-		sqlSession.getMapper(EmployeeMapper.class).set(map);		
+		sqlSession.getMapper(ExampleMapper.class).set(map);		
 	}
 
 	// insert into
 	@Override
 	public void addEmployee(Map<String, String> map) {
-		sqlSession.getMapper(EmployeeMapper.class).add(map);		
+		sqlSession.getMapper(ExampleMapper.class).add(map);		
 		
 	}
 
