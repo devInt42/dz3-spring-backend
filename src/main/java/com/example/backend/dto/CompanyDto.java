9b033993e58f7b63cbfb0201044176c41297c66b
xoxorbrb <tg3782@naver.com>
2023-01-07 16:43:26 +0900

    우편번호, 외국인 여부 컬럼 추가

--- src/main/java/com/example/backend/dto/CompanyDto.java
+++ src/main/java/com/example/backend/dto/CompanyDto.java
@@ -20,5 +20,7 @@ public class CompanyDto {
 	private Date companyEstablish;
 	private Date companyClosingDay;
 	private String companyFax;
+	private String companyZipCode;
+	private String companyForeigner;
 	private int flag;
 }
