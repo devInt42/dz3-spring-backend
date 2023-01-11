d1c078353c4cde0b02df4a380025c9f6168f60df
xoxorbrb <xoxorbrb>
2023-01-11 10:43:53 +0900

    flag 컬럼 변경 및 개인 법인 구분 컬럼 추가

--- src/main/java/com/example/backend/dto/CompanyDto.java
+++ src/main/java/com/example/backend/dto/CompanyDto.java
@@ -22,5 +22,6 @@ public class CompanyDto {
 	private String companyFax;
 	private String companyZipCode;
 	private String companyForeigner;
-	private int flag;
+	private String useYN;
+	private String pcBuisness;
 }
