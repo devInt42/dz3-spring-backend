0774a0bddcadc2f267d1e78a7b16fb3b01351e31
xoxorbrb <tg3782@naver.com>
2023-01-12 22:07:55 +0900

    최신화를 위한 커밋

--- src/main/java/com/example/backend/mapper/companymapper.java
+++ src/main/java/com/example/backend/mapper/companymapper.java
@@ -1,14 +1,5 @@
 package com.example.backend.mapper;
 
-import java.util.List;
-
-import com.example.backend.dto.CompanyDto;
-
 public interface CompanyMapper {
-	List<CompanyDto> getallCompanyInfo();
-	CompanyDto getCompanyInfo(int companySeq);
-	int DupliCheck(int companyCode);
-	void insertCompanyInfo( CompanyDto dto);
-	void updateCompany(CompanyDto dto);
-	void DeleteCompany(int companySeq);
+ 
 }
