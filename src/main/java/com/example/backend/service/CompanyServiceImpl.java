71b7802e0c61ab2f772c5ff741745a54f1a196ac
xoxorbrb <tg3782@naver.com>
2023-01-13 14:46:17 +0900

    z

--- src/main/java/com/example/backend/service/CompanyServiceImpl.java
+++ src/main/java/com/example/backend/service/CompanyServiceImpl.java
@@ -1,11 +1,51 @@
 package com.example.backend.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.backend.dto.CompanyDto;
+
 @Component
 public class CompanyServiceImpl implements CompanyService {
+
+	@Override
+	public List<CompanyDto> getCompanyInformation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CompanyDto getCompanyInfo(int companySeq) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int DupliCheck(int companyCode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void insertCompany(CompanyDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCompany(CompanyDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteCompany(int companySeq) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 //	@Autowired
 //	private SqlSessionTemplate sqlSession;
