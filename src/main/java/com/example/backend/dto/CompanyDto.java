9cdf7b273b7b41583db9b7cdd1208a661d9a1230
xoxorbrb <tg3782@naver.com>
2023-01-13 14:49:59 +0900

    dto수정

---
+++ src/main/java/com/example/backend/dto/CompanyDto.java
@@ -0,0 +1,27 @@
+package com.example.backend.dto;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class CompanyDto {
+	private int companySeq;
+	private int companyCode;
+	private String companyName;
+	private String companyBusiness;
+	private String companyItem;
+	private String companyCall;
+	private String companyRegist;
+	private String companyCorporate;
+	private String companyPresident;
+	private String companyHomepage;
+	private String companyAddr;
+	private Date companyEstablish;
+	private Date companyClosingDay;
+	private String companyFax;
+	private String companyZipCode;
+	private String companyForeigner;
+	private String useYN;
+	private String pcBuisness;
+}
