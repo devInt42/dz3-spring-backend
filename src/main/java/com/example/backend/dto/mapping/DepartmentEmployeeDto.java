91ee430b5f3b6962a0b49b9f3750fb69106aa34d
integer <integer402@gmail.com>
2023-02-07 14:50:14 +0900

    title 삭제

--- src/main/java/com/example/backend/dto/mapping/DepartmentEmployeeDto.java
+++ src/main/java/com/example/backend/dto/mapping/DepartmentEmployeeDto.java
@@ -10,9 +10,7 @@ public class DepartmentEmployeeDto {
 	private int workplaceSeq;
 	private int departmentSeq;
 	private int companySeq;
-	private int subDepartmentSeq;
 	private String companyCode;
-	private String title;
 	private String employeeName;
 	private String employeeId;
 	private String employeePh;
@@ -27,6 +25,8 @@ public class DepartmentEmployeeDto {
 	private int authSeq;
 	private String duty;
 	private String position;
+	private String mainComapny;
+	private String mainDepartment;
 	private int Page;
 	private int startPgNum;
 	private int endPgNum;
